224b6ce153e60ca3f12bd504ca83d3b9d81d69ef
Joshua Clarence Sadsad <joshsad2@gmail.com>
2019-08-01 01:14:12 +0000

    Automatically backed up by Learn

--- db/migrate/20190801011337_create_authors.rb
+++ db/migrate/20190801011337_create_authors.rb
@@ -3,7 +3,7 @@ class CreateAuthors < ActiveRecord::Migration[5.0]
     create_table :authors do |t|
       t.string :name
       t.string :genre
-      t.text :bii
+      t.text :bio
 
       t.timestamps
     end
