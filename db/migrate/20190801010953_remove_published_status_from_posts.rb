a1700a831613e0fc264a4e931e17fb60d701c9c0
Joshua Clarence Sadsad <joshsad2@gmail.com>
2019-08-01 01:10:23 +0000

    Automatically backed up by Learn

--- db/migrate/20190801010953_remove_published_status_from_posts.rb
+++ db/migrate/20190801010953_remove_published_status_from_posts.rb
@@ -1,5 +1,5 @@
 class RemovePublishedStatusFromPosts < ActiveRecord::Migration[5.0]
   def change
-    remove_column :posts, :published_status, :String
+    remove_column :posts, :published_status, :string
   end
 end
